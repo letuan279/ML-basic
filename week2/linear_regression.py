93b2038c6f92f0c7d4650ca1afaa8a9ac8bb3af6
LeTuan279 <87324129+letuan279@users.noreply.github.com>
2023-04-24 09:49:43 +0700

    week2

---
+++ week2/linear_regression.py
@@ -0,0 +1,27 @@
+import matplotlib.pyplot as plt
+import numpy as np
+from sklearn import datasets, linear_model
+from sklearn.metrics import mean_squared_error, r2_score
+
+X, Y = datasets.load_diabetes(return_X_y=True)
+X = X[:, np.newaxis, 2]
+TEST_SIZE = 20
+
+X_train = X[:-TEST_SIZE]
+X_test = X[-TEST_SIZE:]
+Y_train = Y[:-TEST_SIZE]
+Y_test = Y[-TEST_SIZE:]
+
+reg = linear_model.LinearRegression()
+reg.fit(X_train, Y_train)
+Y_pred = reg.predict(X_test)
+
+print("w = ", reg.coef_)
+print("Mean squared error: ", mean_squared_error(Y_test, Y_pred))
+print("Coefficient of determination: ", r2_score(Y_test, Y_pred))
+
+
+plt.scatter(X_test, Y_test, c='black')
+plt.plot(X_test, Y_pred, c='blue', linewidth=3)
+
+plt.show()
